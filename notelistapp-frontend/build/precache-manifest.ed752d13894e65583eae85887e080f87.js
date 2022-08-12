828c7a2d64ed7167e23483d9f2ba58b69b48b8fb
OneSetCode <96799944+OneSetCode@users.noreply.github.com>
2022-08-12 15:09:57 -0700

    init

---
+++ notelistapp-frontend/build/precache-manifest.ed752d13894e65583eae85887e080f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad91862deaeb007191cfc67428f399d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a082fa25b6a7e0ee775",
+    "url": "./static/css/main.82d09655.chunk.css"
+  },
+  {
+    "revision": "85f1c8a096148814a23c",
+    "url": "./static/js/2.4d31561a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4d31561a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a082fa25b6a7e0ee775",
+    "url": "./static/js/main.bf29d34f.chunk.js"
+  },
+  {
+    "revision": "00ba28359098845cda8b",
+    "url": "./static/js/runtime-main.3287d09a.js"
+  }
+]);
